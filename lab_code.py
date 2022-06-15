70e28ba941eba28207f084e300213f2d7e55ffcc
huangweijing <huangweijinglove@gmail.com>
2022-06-15 23:20:28 +0900

    today's work. 2022.6.15(Tue) by Weijing

--- lab_code.py
+++ lab_code.py
@@ -1,10 +1,46 @@
 bbb = "abcdadf"
 
-def m(t:str):
-    for i in t:
-        print(t.count(i))
-        if t.count(i) == 1:
-            return i
+# def m(t:str):
+#     for i in t:
+#         print(t.count(i))
+#         if t.count(i) == 1:
+#             return i
+#         else:
+#             return "no"
+# print(m(bbb))
+
+class Solution:
+    def __init__(self):
+        self.largest_tbl: list[int] = []
+        self.cache = [None] * 10000
+
+    def lis(self, arr: list[int], arr_len) -> list[int]:
+        if arr_len in self.cache:
+            return self.cache[arr_len]
+        if arr_len == 1:
+            return [arr[0]]
+
+        # print(arr, self.largest_tbl, arr_len - 1)
+        if arr[arr_len - 1] == self.largest_tbl[arr_len - 1]:
+            n_1_result = self.lis(arr, arr_len - 1)
+            result = n_1_result.copy()
+            result.append(arr[arr_len - 1])
         else:
-            return "no"
-print(m(bbb))
+            result = self.lis(arr, arr_len - 1).copy()
+
+        self.cache[arr_len] = result
+        return result
+
+    def get_list(self, arr: list[int]):
+        maximum = -1
+        for i in arr:
+            if i > maximum:
+                maximum = i
+            self.largest_tbl.append(maximum)
+        # print(self.largest_tbl)
+        # print(self.lis(arr, len(arr)))
+        return self.lis(arr, len(arr))
+
+sol = Solution()
+r = sol.get_list([1, 4, 2, 7, 8, 9, 2, 1, 3, 8, 6, 5])
+print(r)
